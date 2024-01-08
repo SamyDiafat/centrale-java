e28c71ab5ca22e36b8d463d1d97f843055f999de
Samy Diafat <sami.diafat@outlook.fr>
2024-01-08 10:08:06 +0100

    ajout branche samy

--- cli/src/main/java/org/centrale/cli/App.java
+++ cli/src/main/java/org/centrale/cli/App.java
@@ -1,16 +1,11 @@
 package org.centrale.cli;
 
-import java.util.Scanner;
-
 public class App {
 
     public static void main(String[] args) {
         System.out.println("welcome to the app");
+        JeuUnTour Partie1 = new JeuUnTour("Samy","Lucas");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Veuillez saisir votre choix");
-        int choix = sc.nextInt();
-        System.out.println(choix);
 
     }
 
