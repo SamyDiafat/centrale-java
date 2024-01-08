d27bdee93fbeccd59ee9007e903944cb4be595fa
Samy Diafat <sami.diafat@outlook.fr>
2024-01-08 10:16:58 +0100

    Ajout jeu multi tour

--- cli/src/main/java/org/centrale/cli/App.java
+++ cli/src/main/java/org/centrale/cli/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("welcome to the app");
         JeuUnTour Partie1 = new JeuUnTour("Samy","Lucas");
-
+        JeuMultiTours Partie2 = new JeuMultiTours("Samy","Lucas");
 
     }
 
